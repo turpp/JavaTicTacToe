322d2963c1828fc1c3f5ca347d49c4d33c5eba82
turpp <white.tripp@icloud.com>
2021-07-31 23:20:41 -0500

    added scanner to get input from the user and saved it to pos

--- src/com/company/TicTacToe.java
+++ src/com/company/TicTacToe.java
@@ -1,5 +1,8 @@
 package com.company;
 
+
+import java.util.Scanner;
+
 public class TicTacToe {
 
     public static void main(String[] args) {
@@ -11,6 +14,12 @@ public class TicTacToe {
                 {' ','|',' ', '|', ' '}};
 
         printGameBoard(gameBoard);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your placement (1-9):");
+        int pos = scan.nextInt();
+
+        System.out.println(pos);
 
 
     }
